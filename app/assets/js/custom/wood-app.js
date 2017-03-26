9ae1a613ea72eb1052e4c78953547205b17fdf86
Paweł <p.schwalkowski@gmail.com>
2017-03-26 12:55:06 +0200

    SVG animations

--- app/assets/js/custom/wood-app.js
+++ app/assets/js/custom/wood-app.js
@@ -2,22 +2,47 @@ var APP_MODULE = function($) {
 
     var publicObject = {
 
-      settings: {
-
-      },
-
-      elements: {
-
-      },
-
       init: function() {
 
-      }
+      },
+
+			initScreenScroll: function() {
+				this.animateSectionDecorators();
+			},
+
+			/**
+			 * Add class to decorators which will animate them
+			 * @return {void}
+			 */
+			animateSectionDecorators: function() {
+				var decorators = $('.section-decorator');
+				var _self = this;
+				decorators.each(function() {
+					if (_self._isInViewport(this)) {
+						$('svg', this).addClass('section-decorator-load');
+					}
+				});
+			},
+
+			/**
+			 * Check if element is in viewport
+			 * @see http://stackoverflow.com/questions/20791374/jquery-check-if-element-is-visible-in-viewport#answer-40658647
+			 * @param  {DOM Element} element
+			 * @return boolean
+			 */
+			_isInViewport: function(element) {
+		    var elementTop = $(element).offset().top;
+		    var elementBottom = elementTop + $(element).outerHeight();
+
+		    var viewportTop = $(window).scrollTop();
+		    var viewportBottom = viewportTop + $(window).height();
+
+		    return elementBottom > viewportTop && elementTop < viewportBottom;
+			}
 
     };
 
     return publicObject;
-
   };
 
 var app = new APP_MODULE(jQuery);
@@ -25,4 +50,8 @@ var app = new APP_MODULE(jQuery);
   $(document).ready(function() {
     app.init();
   });
+
+	$(window).on('scroll', function() {
+		app.initScreenScroll();
+	});
 })(jQuery);
